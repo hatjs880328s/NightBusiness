4aaec9adb7c78e58301c6d0bb104b8b0cdc2535c
Noah_Shan <shanwzh@inspur.com>
2020-07-01 15:23:20 +0800

    = init pod =

---
+++ NightBusiness/NightBusiness/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  *******************************************
+//  
+//  ViewController.swift
+//  NightBusiness
+//
+//  Created by Noah_Shan on 2020/7/1.
+//  Copyright © 2018 Inpur. All rights reserved.
+//  
+//  *******************************************
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
